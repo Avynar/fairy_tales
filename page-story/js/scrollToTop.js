b3df1833c92c147e9a217372ace57c3ea770ec6c
Avynar <igorexa.52.52@gmail.com>
2023-02-07 11:15:24 +0700

    full refactor

--- page-story/js/scrollToTop.js
+++
@@ -1,5 +0,0 @@
-export default function scrollToTop(scrollback, stories, album){
-    scrollback.classList.add("dn");
-    stories.classList.add("stories--active");
-    album.classList.remove("album--active");
-};
